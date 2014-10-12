b7101e39211f60aee695b306dd6e2a46a5c64adb
Anthony Dagati <tdagati.cs@gmail.com>
2014-10-12 12:24:39 -0700

    First commit

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -11,7 +11,13 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+       
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+    
     }
     return 0;
 }
